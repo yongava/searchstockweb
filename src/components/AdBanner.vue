c0cb45a5c8591c3d316ab1f4aeb20436067f9ee9
yongava <>
2023-08-15 16:32:24 +0700

    add ads

--- src/components/AdBanner.vue
+++ src/components/AdBanner.vue
@@ -1,25 +1,18 @@
 <template>
-  <div class="ad-container">
-      <ins class="adsbygoogle"
-          style="display:block"
-          :data-ad-client="adClient"
-          :data-ad-slot="adSlot"
-          data-ad-format="auto"
-          data-full-width-responsive="true">
-      </ins>
+  <div class="ads">
+    <!-- Insert the ad component markup here. -->
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      adClient: "ca-pub-3199261308247865",
-      adSlot: "1001" // replace with your ad slot ID
-    };
-  },
+  name: 'AdComponent',
   mounted() {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   },
-}
+};
 </script>
+
+<style scoped>
+/* Insert your ad component styles here. */
+</style>
