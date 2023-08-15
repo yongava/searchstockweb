d41a341eb069df02509a389d7d7f5a885a630486
yongava <>
2023-08-15 19:04:37 +0700

    improve perf

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,6 @@ export default {
   components: {
     SearchCard,
     AdBanner
-  },
-  mounted() {
-    const script = document.createElement('script')
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3199261308247865'
-    script.async = true
-    script.setAttribute('crossorigin', 'anonymous')
-    document.head.appendChild(script)
   }
 }
 
