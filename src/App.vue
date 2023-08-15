c513f5094307c94a7d53b791cc4d74369d76ace2
yongava <>
2023-08-15 20:28:48 +0700

    ads

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,6 @@ export default {
     AdBanner
   }
 }
-
 </script>
 
 <style>
--- src/components/AdBanner.vue
+++ src/components/AdBanner.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="ads">
-    <!-- Insert the ad component markup here. -->
+  <div id="ad-container">
+    <amp-ad 
+      width="100vw" 
+      height="320"
+      type="adsense"
+      data-ad-client="ca-pub-3199261308247865"
+      data-ad-slot="7420360525"
+      data-auto-format="rspv"
+      data-full-width="">
+      <div overflow=""></div>
+    </amp-ad>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AdBanner',
-  mounted() {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  },
-};
+}
 </script>
 
 <style scoped>
-/* Insert your ad component styles here. */
+#ad-container {
+  /* Style your ad container if needed */
+}
 </style>
