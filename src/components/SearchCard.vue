ac8be0b9f3d97a89ee5eb7287d991b4aee9f07e8
yongava <>
2023-08-16 18:11:35 +0700

    update

--- src/components/SearchCard.vue
+++ src/components/SearchCard.vue
@@ -106,7 +106,7 @@
 import axios from 'axios';
 import mixpanel from 'mixpanel-browser';
 
-mixpanel.init("e0e5a1748e7a2c12d17361e1c381a326", { debug: true, track_pageview: true, persistence: 'localStorage' });
+mixpanel.init("f4a21b2032c39127a1cfeb503951376d", { debug: true, track_pageview: true, persistence: 'localStorage' });
 
 // Track an event. It can be anything, but in this example, we're tracking a Sign Up event.
 mixpanel.track('Open Page', {
